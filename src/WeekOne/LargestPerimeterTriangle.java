da4d3d857e135e0c150074e439982ba757b951d9
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-01-09 17:20:31 +0300

    Largest perimeter triangle

--- src/WeekOne/LargestPerimeterTriangle.java
+++ src/WeekOne/LargestPerimeterTriangle.java
@@ -1,4 +1,18 @@
 package WeekOne;
 
+import java.util.Arrays;
+
 public class LargestPerimeterTriangle {
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        int max=0;
+        for(int i=0;i<A.length-2;i++){
+            if(A[i]+A[i+1]>A[i+2] && A[i]+A[i+1]+A[i+2] >max){
+                max=A[i]+A[i+1]+A[i+2];
+            }
+        }
+
+        return max;
+
+    }
 }
