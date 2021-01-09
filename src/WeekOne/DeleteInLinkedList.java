57e0df35a822352e46d3b83bd5fc7a89d0aa0a54
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-01-09 19:13:45 +0300

    delete node from linkedList

---
+++ src/WeekOne/DeleteInLinkedList.java
@@ -0,0 +1,31 @@
+package WeekOne;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DeleteInLinkedList {
+    public static void main(String[] args) {
+        Node linkedList = new Node(3, new Node(4, new Node(0, new Node(8, new Node(10)))));
+        System.out.println(deleteLinkedList(linkedList,88));
+    }
+
+
+;
+
+    public static boolean deleteLinkedList(Node linkedList, int data) {
+        Node head = linkedList;
+        if (linkedList.val == data) {
+            head = linkedList.next;
+            return true;
+        }
+        while (linkedList != null) {
+            if (linkedList.next != null && linkedList.next.val == data) {
+                  linkedList.next=linkedList.next.next;
+                  return true;
+            }
+            linkedList=linkedList.next;
+
+        }
+        return  false;
+    }
+}
