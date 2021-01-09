91eeb11950ef4702236e6fd42729ade4dcc34a40
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-01-10 00:15:31 +0300

    removing duplicates from sorted array

---
+++ src/WeekOne/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,21 @@
+package WeekOne;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int total=nums.length;
+        int previous=nums[0];
+        int index=1;
+
+        for(int i=1;i<nums.length;i++){
+            if(previous!=nums[i]){
+                nums[index]=nums[i];
+                previous=nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
