7b2e4256e5767638b79afcc0f9929323ccdbfcd0
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-01-15 14:16:45 +0300

    stack using array

--- src/WeekTwo/QueueWithStack.java
+++ src/WeekTwo/QueueWithStack.java
@@ -1,4 +1,10 @@
 package WeekTwo;
 
+import java.util.Stack;
+
 public class QueueWithStack {
+    public static void main(String[] args) {
+        Stack<Object> stack1=new Stack<>();
+
+    }
 }
--- src/WeekTwo/stackWithArray.java
+++ src/WeekTwo/stackWithArray.java
@@ -1,4 +1,26 @@
 package WeekTwo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class stackWithArray {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> stack=new ArrayList<>();
+        int num=1;
+        int index=0;
+        while(index<target.length){
+            if(target[index]==num){
+                stack.add("Push");
+                index++;
+            }
+            else{
+                stack.add("Push");
+                stack.add("Pop");
+
+            }
+            num++;
+        }
+        return stack;
+    }
+
 }
