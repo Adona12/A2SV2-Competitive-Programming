7e2e0ce3020af1c6cb49d8f0ce5928cf585dbb99
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-01-16 17:18:55 +0300

    find merge point of two lists

--- src/contest/FindMergeSortOfTwoLists.java
+++ src/contest/FindMergeSortOfTwoLists.java
@@ -1,21 +1,32 @@
 package contest;
 
+import java.util.Stack;
+
 public class FindMergeSortOfTwoLists {
     static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
-        SinglyLinkedListNode node1=head1;
-        SinglyLinkedListNode node2=head2;
-        while(node1!=null){
-            node2=head2;
-            while(node2!=null){
-                if(node1==node2){
-                    return node1.data;
-                }
-                node2=node2.next;
-            }
-            node1=node1.next;
-        }
-        return 0;
 
+        Stack<SinglyLinkedListNode> stack1=new Stack<SinglyLinkedListNode>();
+        Stack<SinglyLinkedListNode> stack2=new Stack<SinglyLinkedListNode>();
+        while(head1!=null){
+            stack1.push(head1);
+            head1=head1.next;
+        }
+        while(head2!=null){
+            stack2.push(head2);
+            head2=head2.next;
+        }
+        int temp=0;
+        while(!stack1.isEmpty() && !stack2.isEmpty()){
+
+            if(stack1.peek()==stack2.peek()){
+                temp=stack1.pop().data;
+                stack2.pop();
+            }
+            else{
+                break;
+            }
+        }
+        return temp;
     }
 
 }
